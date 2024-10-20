5e09daf76705c00ae234669935110549125e0e01
unknown <daffamusyaffa10@gmail.com>
2024-10-20 20:46:39 +0700

    OOP DASAR pada PHP #7 - Inheritance (Bagian 2)

---
+++ oop/OOP DASAR pada PHP #7 - Inheritance (Bagian 2)/Mobil.php
@@ -0,0 +1,27 @@
+<?php
+
+class Mobil
+{
+  public $nama, $merk, $warna, $kecepatanMaksimal, $jumlahPenumpang;
+
+  public function tambahKecepatan()
+  {
+    return "Kecepatan bertambah";
+  }
+}
+
+class MobilSport extends Mobil
+{
+  public $turbo = false;
+
+  public function jalankanTurbo()
+  {
+    $this->turbo = true;
+    return "Turbo dijalankan!";
+  }
+}
+
+$mobil = new MobilSport();
+echo $mobil->tambahKecepatan();
+echo "<br>";
+echo $mobil->jalankanTurbo();
