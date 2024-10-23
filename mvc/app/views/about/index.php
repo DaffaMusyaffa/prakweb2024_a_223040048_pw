42572fdafbc77c219888622426c248fbc0d9a982
unknown <daffamusyaffa10@gmail.com>
2024-10-23 11:32:55 +0700

    Membuat Aplikasi MVC dengan PHP #6 Assets

--- mvc/app/views/about/index.php
+++ mvc/app/views/about/index.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Halaman About</title>
-</head>
-
-<body>
-  <h1>About Me</h1>
+<div class="container">
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL; ?>/img/profile.jpeg" alt="Muhammad Daffa Musyaffa" width="200" class="rounded-circle shadow">
   <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?></p>
-</body>
-
-</html>
+</div>
---
+++ mvc/public/js/script.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".tombolTambahData").on("click", function () {
+    $("#formModalLabel").html("Tambah Data Mahasiswa");
+  });
+  $(".tampilModalUbah").on("click", function () {
+    $("#formModalLabel").html("Ubah Data Mahasiswa");
+  });
+});
